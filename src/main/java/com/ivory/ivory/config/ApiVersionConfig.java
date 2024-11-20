a2f5efa4c44b07cafd91f246bd67b80821e817fe
GiHwan2 <sandwich5011@naver.com>
2024-11-20 20:15:17 +0900

    feat(ApiVersionConfig): /api/v1 프리픽스 설정

--- src/main/java/com/ivory/ivory/config/ApiVersionConfig.java
+++ src/main/java/com/ivory/ivory/config/ApiVersionConfig.java
@@ -1,4 +1,14 @@
 package com.ivory.ivory.config;
 
-public class ApiVersionConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiVersionConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api/v1", clazz -> clazz.getPackageName().startsWith("com.ivory.ivory.controller"));
+    }
 }
