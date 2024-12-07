2fa4c4f826e7bb370ac4e1a0c76c5273b1634f40
Juye0nLee <dlwndus708@naver.com>
2024-12-07 18:43:03 +0900

    🎨 학부모ver 신청 목록 최신순 정렬 수정

--- src/main/java/com/ivory/ivory/repository/ApplyRepository.java
+++ src/main/java/com/ivory/ivory/repository/ApplyRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Repository
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
-    List<Apply> findAllByChild_Id(Long childId);
+    List<Apply> findAllByChild_IdOrderByCreateAtDesc(Long childId);
     Optional<Apply> findById (Long applyId);
     Apply findFirstByChild_IdOrderByCreateAt(Long childID);
     Optional<Apply> findFirstByStatusOrderByCreateAtDesc(Status status);
